b8c98bf20286b88f74fedf3fe01f4ec9fc062d65
chromaticpipe <127907268+chromaticpipe@users.noreply.github.com>
2024-12-22 17:39:01 -0600

    Revert "avoid branch prediction slowdowns yadayada"
    This reverts commit 1bd03ea408e7241997f7e39b19b75e7d750ffecf.

--- src/r_main.c
+++ src/r_main.c
@@ -253,7 +253,7 @@ static void FlipCam2_OnChange(void)
 //
 // killough 5/2/98: reformatted
 //
-INT32 R_PointOnSide(fixed_t x, fixed_t y, node_t *restrict node)
+INT32 R_PointOnSide(fixed_t x, fixed_t y, node_t *node)
 {
 	if (!node->dx)
 		return x <= node->x ? node->dy > 0 : node->dy < 0;
@@ -265,14 +265,13 @@ INT32 R_PointOnSide(fixed_t x, fixed_t y, node_t *restrict node)
 	y -= node->y;
 
 	// Try to quickly decide by looking at sign bits.
-	// also use a mask to avoid branch prediction
-	INT32 mask = (node->dy ^ node->dx ^ x ^ y) >> 31;
-	return (mask & ((node->dy ^ x) < 0)) |  // (left is negative)
-		(~mask & (FixedMul(y, node->dx>>FRACBITS) >= FixedMul(node->dy>>FRACBITS, x)));
+	if ((node->dy ^ node->dx ^ x ^ y) < 0)
+		return (node->dy ^ x) < 0;  // (left is negative)
+	return FixedMul(y, node->dx>>FRACBITS) >= FixedMul(node->dy>>FRACBITS, x);
 }
 
 // killough 5/2/98: reformatted
-INT32 R_PointOnSegSide(fixed_t x, fixed_t y, seg_t *restrict line)
+INT32 R_PointOnSegSide(fixed_t x, fixed_t y, seg_t *line)
 {
 	fixed_t lx = line->v1->x;
 	fixed_t ly = line->v1->y;
@@ -289,10 +288,9 @@ INT32 R_PointOnSegSide(fixed_t x, fixed_t y, seg_t *restrict line)
 	y -= ly;
 
 	// Try to quickly decide by looking at sign bits.
-	// also use a mask to avoid branch prediction
-	INT32 mask = (ldy ^ ldx ^ x ^ y) >> 31;
-	return (mask & ((ldy ^ x) < 0)) |  // (left is negative)
-		(~mask & (FixedMul(y, ldx>>FRACBITS) >= FixedMul(ldy>>FRACBITS, x)));
+	if ((ldy ^ ldx ^ x ^ y) < 0)
+		return (ldy ^ x) < 0;          // (left is negative)
+	return FixedMul(y, ldx>>FRACBITS) >= FixedMul(ldy>>FRACBITS, x);
 }
 
 //
