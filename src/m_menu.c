6b0367111ac93755ab9237721752de9ac176dcd7
chromaticpipe <127907268+chromaticpipe@users.noreply.github.com>
2025-01-24 17:33:30 -0600

    fuck my fucking chud life

--- src/m_menu.c
+++ src/m_menu.c
@@ -2891,7 +2891,7 @@ void M_Init(void)
 #ifdef HWRENDER
 	// Permanently hide some options based on render mode
 	if (rendermode == render_soft)
-		OP_VideoOptionsMenu[1].status = IT_DISABLED;
+		OP_VideoOptionsMenu[12].status = IT_DISABLED;
 #endif
 
 #ifndef NONET
