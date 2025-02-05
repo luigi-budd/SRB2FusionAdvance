7fe88510a20663b29945fbbd2cd1ccaca0481371
chromaticpipe <127907268+chromaticpipe@users.noreply.github.com>
2025-02-05 16:18:50 -0600

    don't reset precip state twice!!!

--- src/p_mobj.c
+++ src/p_mobj.c
@@ -3852,9 +3852,6 @@ void P_NullPrecipThinker(precipmobj_t *mobj)
 
 void P_SnowThinker(precipmobj_t *mobj)
 { 
-
-    R_ResetPrecipitationMobjInterpolationState(mobj);
-
 	P_CycleStateAnimation((mobj_t *)mobj);
 
 	// adjust height
@@ -3867,8 +3864,6 @@ void P_SnowThinker(precipmobj_t *mobj)
 
 void P_RainThinker(precipmobj_t *mobj)
 { 
-	R_ResetPrecipitationMobjInterpolationState(mobj);
-
 	P_CycleStateAnimation((mobj_t *)mobj);
 
 	if (mobj->state != &states[S_RAIN1])
